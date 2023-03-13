86592008f5314af0ea0ca48e2459474a040d1235
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-03-13 09:11:09 +0900

    ui: 최근 검색 기록 ui 추가

--- app/src/main/java/com/june/android/accountsecurity/SearchFragment.kt
+++ app/src/main/java/com/june/android/accountsecurity/SearchFragment.kt
@@ -3,12 +3,10 @@ package com.june.android.accountsecurity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.june.android.accountsecurity.AppConst.TAG
 import com.june.android.accountsecurity.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -24,6 +22,7 @@ class SearchFragment : Fragment() {
 
         onBackButtonClicked()
         onSearchEditTextviewChanged()
+        onDeleteAllRecentSearchButtonClicked()
     }
 
     private fun onBackButtonClicked() {
@@ -34,15 +33,18 @@ class SearchFragment : Fragment() {
 
     private fun onSearchEditTextviewChanged() {
         binding.fragmentSearchEditTv.addTextChangedListener(object : TextWatcher {
-            //입력하여 변화가 생기기 전에 처리
-            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
-            //변화와 동시에 처리를 해주고자 할 때
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { } //입력하여 변화가 생기기 전에 처리
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { //변화와 동시에 처리를 해주고자 할 때
                 //TODO DB 애서 해당 키워드로 검색
                 Toast.makeText(requireContext(), "$s", Toast.LENGTH_SHORT).show()
             }
-            //입력이 끝났을 때 처리
-            override fun afterTextChanged(s: Editable?) { }
+            override fun afterTextChanged(s: Editable?) { } //입력이 끝났을 때 처리
         })
+    }
+
+    private fun onDeleteAllRecentSearchButtonClicked() {
+        binding.fragmentSearchTvDeleteAllRecentSearch.setOnClickListener {
+            Toast.makeText(requireContext(), "최근 검색 기록 모두 지우기", Toast.LENGTH_SHORT).show()
+        }
     }
 }
